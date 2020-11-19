eaf85f9830e1c363edd80712cc430c5c412dc8c9
nanQiang1997 <58358421+nanQiang1997@users.noreply.github.com>
2020-11-20 00:20:46 +0800

    rem配置

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <van-button type="primary">主要按钮</van-button>
+  <van-card
+    num="2"
+    price="2.00"
+    desc="描述信息"
+    title="商品标题"
+    thumb="https://img.yzcdn.cn/vant/ipad.jpeg"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import { onMounted } from "vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  setup() {
+    onMounted(() => {
+      (
+        //屏幕适应
+        function (win, doc) {
+          if (!win.addEventListener) return;
+          // eslint-disable-next-line no-unused-vars
+          var htmltext = document.documentElement;
+          function setFont() {
+            var htmltext = document.documentElement;
+            var k = 750;
+            htmltext.style.fontSize = (htmltext.clientWidth / k) * 100 + "px";
+          }
+          setFont();
+          setTimeout(function () {
+            setFont();
+          }, 300);
+          doc.addEventListener("DOMContentLoaded", setFont, false);
+          win.addEventListener("resize", setFont, false);
+          win.addEventListener("load", setFont, false);
+        }
+      )(window, document);
+    });
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import { Card, Button, Toast } from 'vant'
 import App from './App.vue'
+import './static/css/base.css'
+import 'vant/lib/index.css'; // 全局引入样式
 
-createApp(App).mount('#app')
+const app = createApp(App) // 创建实例
+
+app.use(Card).use(Button).use(Toast)
+
+app.mount('#app')
