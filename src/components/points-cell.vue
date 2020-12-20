34eaee009aaeb10e3ca68be00450f092c3b70f36
nanQiang1997 <58358421+nanQiang1997@users.noreply.github.com>
2020-12-20 21:12:27 +0800

    cell 组件购物车添加

--- src/components/points-cell.vue
+++ src/components/points-cell.vue
@@ -1,30 +1,68 @@
 
 <template>
   <div class="points-cell">
-      <div class="shop-view">
-        <img class="shop-pic" :src="shopItem.pic" alt="">
-        <!-- <div class="shop-icon"></div> -->
+    <div class="shop-view">
+      <img class="shop-pic" :src="shopItem.pic" alt="" />
+      <!-- <div class="shop-icon"></div> -->
+    </div>
+    <div class="shop-desc">
+      <div class="desc-title">
+        {{ shopItem.descTitle }}
       </div>
-      <div class="shop-desc">
-          <div class="desc-title">
-            {{shopItem.descTitle}}
-          </div>
-          <div class="desc-content">{{shopItem.descContent}}</div>
-      </div>
+      <div class="desc-content">{{ shopItem.descContent }}</div>
+    </div>
+    <template v-if="operate == 'show'">
       <div class="shop-operate" @click="handleOpShow">
-          <div class="points-item">
-              <div class="point-num ed-item">{{shopItem.pointsNum}}</div>
-              <div class="point-text ed-item">points</div>
-          </div>
-          <!-- <div class="points-more">
-              <div class="more-item">
-                 <div class="point-num">{{shopItem.pointsNum}}</div>
-                 <div class="point-text">points</div>
-              </div>
-              <div class="more-icon"></div>
-          </div> -->
+        <div class="points-item">
+          <div class="point-num ed-item">{{ shopItem.pointsNum }}</div>
+          <div class="point-text ed-item">points</div>
+        </div>
+        <!-- <div class="points-more">
+                <div class="more-item">
+                  <div class="point-num">{{shopItem.pointsNum}}</div>
+                  <div class="point-text">points</div>
+                </div>
+                <div class="more-icon"></div>
+            </div> -->
       </div>
-      <div class="mask-opreate" @click="opShow = false" v-if="opShow">
+    </template>
+
+    <!-- 购物车 -->
+    <template v-if="operate == 'shopCar'">
+      <div class="shop-operate shopcar-operate">
+        <div class="shop-item">
+          <div class="point-num ed-item">{{ shopItem.pointsNum }}</div>
+          <div class="point-text ed-item">points</div>
+        </div>
+        <div class="count-bar">
+          <div class="sub">
+            <van-icon class="count-i" name="minus" />
+          </div>
+          <div class="num">1</div>
+          <div class="add"><van-icon class="count-i" name="plus" /></div>
+        </div>
+      </div>
+    </template>
+    <!-- 订单 -->
+    <template v-if="operate == 'order'">
+      <div class="shop-operate shopcar-operate">
+        <div class="shop-item">
+          <div class="point-num ed-item">{{ shopItem.pointsNum }}</div>
+          <div class="point-text ed-item">points</div>
+        </div>
+        <div class="order-status">
+            <!--  -->
+            <div class="no">
+              <img src="../static/images/user/no.png" alt="">
+            </div>
+            <!-- <div class="yes">
+               <img src="../static/images/user/yes.png" alt="">
+            </div> -->
+        </div>
+      </div>
+    </template>
+    <!--  admin操作 -->
+    <!-- <div class="mask-opreate" @click="opShow = false" v-if="opShow">
          <div class="shop-operate">
            <div class="op-conten">
               <div class="op-item edit" @click.stop="handleEditOrDelete(1)">
@@ -36,155 +74,206 @@
            </div>
         
          </div>
-      </div>
-  </div>  
+      </div> -->
+  </div>
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue'
+import { reactive, toRefs } from "vue";
 export default {
-  props:{
-    shopItem:{
-      type:Object,
-      default:{
-        pic:require('../static/images/login/j1.png'),
-        icon:'',
-        descTitle:'A bunch of flowers',
-        descContent:'Flower express 33 rose bouquel gift box champagne rose birthday gift proposal to girltriend ',
-        pointsNum:0,
-      }
+  props: {
+    shopItem: {
+      type: Object,
+      default: {
+        pic: require("../static/images/login/j1.png"),
+        icon: "",
+        descTitle: "A bunch of flowers",
+        descContent:
+          "Flower express 33 rose bouquel gift box champagne rose birthday gift proposal to girltriend ",
+        pointsNum: 0,
+      },
     },
-    shopIndex:{
-      type:Number,
-      default:0
-    }
+    operate: {
+      type: String,
+      default: "order",
+    },
+    shopIndex: {
+      type: Number,
+      default: 0,
+    },
   },
-  emits:['handleDelete'],
-  setup(props, ctx){
+  emits: ["handleDelete"],
+  setup(props, ctx) {
     const state = reactive({
-        opShow:false,
-    })
-    
-    const handleOpShow = ()=>{
-      state.opShow = true
-    }
+      opShow: false,
+    });
 
-    const handleEditOrDelete = (type)=>{
-        // 1 编辑  0 删除
-        let operate = {
-          '0':'delete',
-          '1':'edite'
-        }
-        switch (operate[type]) {
-          case 'edite':
-            
-            break;
-          case 'delete':
-            ctx.emit('handleDelete',props.shopIndex)
-            break;
-          default:
-            break;
-        }
-    }
+    const handleOpShow = () => {
+      state.opShow = true;
+    };
+
+    const handleEditOrDelete = (type) => {
+      // 1 编辑  0 删除
+      let operate = {
+        0: "delete",
+        1: "edite",
+      };
+      switch (operate[type]) {
+        case "edite":
+          break;
+        case "delete":
+          ctx.emit("handleDelete", props.shopIndex);
+          break;
+        default:
+          break;
+      }
+    };
     return {
       ...toRefs(state),
       handleOpShow,
-      handleEditOrDelete
-    }
-  }
-}
+      handleEditOrDelete,
+    };
+  },
+};
 </script>
 
 <style scoped>
-@import '../static/css/mixin.css';
-  .points-cell{
-    display: flex;
-    height: 2.04rem;
-    /* background: yellowgreen; */
-    justify-content: space-between;
-    position: relative;
-    margin-bottom: .2rem;
-    border-radius: .14rem;
-    overflow: hidden;
-  }
-  img{
-    width: 100%;
-  }
-  .shop-pic{
-    object-fit: cover;
-  }
-  .shop-view{
-    width: 2.6rem;
-    height: 2.04rem;
-    position: relative;
-    border: 1px solid #392724;
-    /* background-color: teal; */
-  }
-  .shop-desc{
-    padding: .2rem;
-    background-color: #3c2a26;
-    flex: 1;
-    width: 0;
-    border: 1px solid #392724;
-  }
-  .desc-title{
-    font-size: .28rem;
-    color: #dc6841;
-    font-weight: bold;
-    margin-bottom: .2rem;
-  }
-  .desc-content{
-    font-size: .2rem;
-    line-height: 1.2;
-    color: #8d8176;
-  }
-  .shop-operate{
-    width: 1.45rem;
-    height: 100%;
-    background-color: #fb882b;
-    margin-left: auto;
-    overflow: hidden;
-    border: 1px solid #392724;
-  }
-  .points-item{
-    height: 100%;
-  }
-  .point-num{
-    font-size: .6rem;
-    font-weight: bold;
-    text-align: center;
-    color: #52352d;
-    line-height: 2.5;
-  }
-  .point-text{
-    text-align: center;
-    font-size: .3rem;
-  }
-  .mask-opreate{
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-  }
-  .op-conten{
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-  }
-  .op-item{
-    flex: 1;
-    height: 0;
-    color: #fff;
-    font-size: .36rem;
-    font-weight: bold;
-    width: 100%;
-    text-align: center;
-    line-height: 3;
-  }
-  .ed-item{
-    height: 50%;
-  }
-
+@import "../static/css/mixin.css";
+.points-cell {
+  display: flex;
+  height: 2.04rem;
+  /* background: yellowgreen; */
+  justify-content: space-between;
+  position: relative;
+  margin-bottom: 0.2rem;
+  border-radius: 0.14rem;
+  overflow: hidden;
+}
+img {
+  width: 100%;
+  vertical-align: top;
+}
+.shop-pic {
+  object-fit: cover;
+}
+.shop-view {
+  width: 2.6rem;
+  height: 2.04rem;
+  position: relative;
+  border: 1px solid #392724;
+  /* background-color: teal; */
+}
+.shop-desc {
+  padding: 0.2rem;
+  background-color: #3c2a26;
+  flex: 1;
+  width: 0;
+  border: 1px solid #392724;
+}
+.desc-title {
+  font-size: 0.28rem;
+  color: #dc6841;
+  font-weight: bold;
+  margin-bottom: 0.2rem;
+}
+.desc-content {
+  font-size: 0.2rem;
+  line-height: 1.2;
+  color: #8d8176;
+}
+.shop-operate {
+  width: 1.45rem;
+  height: 100%;
+  background-color: #fb882b;
+  margin-left: auto;
+  overflow: hidden;
+  border: 1px solid #392724;
+}
+.shopcar-operate {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent !important;
+}
+.shopcar-operate .ed-item {
+  color: #fff;
+}
+.no{
+  width: .69rem;
+  height: .13rem;
+}
+.yes{
+  width: .67rem;
+  height: .48rem;
+}
+.points-item {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.shop-item{
+  margin-bottom: .4rem;
+}
+.shop-item .point-num {
+  line-height: 1;
+}
+.count-bar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.add , .sub {
+  width: .35rem;
+  height: .35rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  border-radius: 50%;
+  font-size: .28rem;
+}
+.num{
+  font-size: .32rem;
+  font-weight: 700;
+  color: #fff;
+  margin:  0 .14rem;
+}
+.point-num {
+  font-size: 0.6rem;
+  font-weight: bold;
+  text-align: center;
+  color: #52352d;
+  line-height: 2.5;
+}
+.point-text {
+  text-align: center;
+  font-size: 0.3rem;
+}
+.mask-opreate {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
+.op-conten {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.op-item {
+  flex: 1;
+  height: 0;
+  color: #fff;
+  font-size: 0.36rem;
+  font-weight: bold;
+  width: 100%;
+  text-align: center;
+  line-height: 3;
+}
+.ed-item {
+  height: 50%;
+}
 </style>
